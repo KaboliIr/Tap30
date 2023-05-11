ac513c26845ba1eeed2ea06b45ed64d680e175d0
Kaboli.ir <102959560+KaboliIr@users.noreply.github.com>
2023-05-11 21:18:44 +0330

    Add files via upload

---
+++ Tapsi/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Pages/*.html"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'vazirBold':["vazirBold"],
+      'vazirLight':["vazirLight"]
+    }
+  },
+  plugins: [],
+}
